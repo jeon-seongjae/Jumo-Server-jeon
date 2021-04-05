28e4d54f6efe34ad109e9efb66fd2e914311a68a
jeon-seongjae <75592608+jeon-seongjae@users.noreply.github.com>
2021-04-05 16:49:07 +0900

    Update remove.js

--- Controller/Like/remove.js
+++ Controller/Like/remove.js
@@ -13,7 +13,7 @@ module.exports = async (req, res) => {
         ).then(result => {
             like
                 .destroy({
-                    where: { makgeolli_id: result.dataValues.id, user_id: res.locals.userId } //테이블에 고유 아이디가 클라한테 있으면 그걸로 찾으면 될듯하다
+                    where: { makgeolli_id: result.dataValues.id, user_id: res.locals.userId } 
                 })
         })
 }
