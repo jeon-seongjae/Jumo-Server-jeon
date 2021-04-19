073fc6bd48600516d70bf5e130cb1d6f9af93be0
jeon-seongjae <tjdwoek62@google.com>
2021-04-19 15:15:40 +0900

    fix #51 - edit:Controller_makgeolli수정

--- Controller/makgeolli/info.js
+++ Controller/makgeolli/info.js
@@ -1,9 +1,13 @@
 const { makgeolli } = require('../../models');
 
 module.exports = async (req, res) => {
+    const { pageNum } = req.body;
     await makgeolli
-        .findAll()
+        .findAll({
+            offset: pageNum, limit: 1
+        })
         .then(result => {
             res.status(200).json({ data: result, message: "ok" });
         })
 }
+
