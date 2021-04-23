f88b210ee250653edd7f78cda572aeb0a93390a1
jeon-seongjae <tjdwoek62@google.com>
2021-04-23 12:43:50 +0900

    Fix #71 - edit:Controller_makgeolli,brewery코드수정

--- Controller/makgeolli/search.js
+++ Controller/makgeolli/search.js
@@ -2,15 +2,14 @@ const { makgeolli, sequelize } = require('../../models');
 const { Op } = require('sequelize');
 
 module.exports = async (req, res) => {
-    const { query, pageNum, limit } = req.query;
+    const { query, pageNum } = req.query;
     const pageNum2 = Number(pageNum);
-    const limit2 = Number(limit);
     await makgeolli
         .findAll({
             where: {
                 name: { [Op.like]: "%" + query + "%" }
             },
-            offset: pageNum2, limit: limit2
+            offset: pageNum2, limit: 1
         })
         .then(result => {
             res.status(200).json({ data: result });
